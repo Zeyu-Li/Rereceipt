d16a9146afbb75c6a69ab1eb083bad9d560638f1
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-15 14:07:15 -0600

    started structure

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Main } from "./pages/main";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Main />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ pages/main.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Text } from "react-native";
+import { Button } from "../components/button";
+
+export const Main = () => {
+  return (
+    <>
+      <Text>Main</Text>
+      <Button />
+    </>
+  );
+};
