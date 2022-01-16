32c622a3b4bc744f0a79237dbcdee2172fb47582
gokul887 <69052109+gokul887@users.noreply.github.com>
2022-01-16 09:54:52 -0700

    AsyncStorage

--- pages/manual.jsx
+++ pages/manual.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Pressable } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 import styles from "../styles/styles";
@@ -7,13 +8,27 @@ export const Manual = ({ setManualPage }) => {
   const [item, setItem] = useState("");
   const [price, setPrice] = useState("");
   const [date, setDate] = useState(new Date());
+  const [data, setData] = useState([])
 
-  const submit = () => {
+  const submit = async () => {
     // submit data here
     console.log("submitted");
+    const receiptData = {id: Date.now(), item: item, price: price, date: date}
+    const updatedData = [...data, receiptData]
+    setData(updatedData)
+    await AsyncStorage.setItem('data', JSON.stringify(updatedData))
     // if success return
     setManualPage(false);
   };
+
+  const findData = async () => {
+    const result = await AsyncStorage.getItem('data')
+    result !== null? setData(JSON.parse(result)) : setData(null)
+
+  }
+  useEffect(() => {
+    findData()
+  }, [])
 
   return (
     <>
