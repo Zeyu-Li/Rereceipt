66ae68c1d2e3332a699092dd7a67492345e68f2c
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-15 22:02:06 -0600

    updated table

--- components/common/Table.jsx
+++ components/common/Table.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { DataTable, Text } from "react-native-paper";
+import { Button, DataTable, Text } from "react-native-paper";
 import styles from "../../styles/styles";
 
 export const OverviewTable = () => {
+  const [sortedData, setSortedData] = useState([
+    ["item 1", new Date(), 10.0],
+    ["item 2", new Date(), 20.01],
+  ]);
   const [data, setData] = useState([]);
+  const [ascending, setAscending] = useState(true);
   const months = [
     "Jan",
     "Feb",
@@ -20,11 +25,13 @@ export const OverviewTable = () => {
   ];
 
   useEffect(() => {
-    const date = new Date();
-    setData([
-      ["item 1", date, 10.0],
-      ["item 2", date, 20.01],
-    ]);
+    // get data here?
+    // sort by date here
+    setData(
+      sortedData.sort((a, b) => {
+        return b[0] - a[0];
+      })
+    );
   }, []);
 
   return (
@@ -35,6 +42,16 @@ export const OverviewTable = () => {
         </DataTable.Title>
         <DataTable.Title numeric>
           <Text style={styles.dataHeader}>Date</Text>
+          {/* <Button
+            icon={ascending ? "arrow-up-bold" : "arrow-down-bold"}
+            color="white"
+            onPress={() => setAscending(!ascending)}
+            style={{ padding: 0, height: "48px", margin: 0 }}
+          >
+            <Text style={[styles.dataHeader, { padding: 0, margin: 0 }]}>
+              Date
+            </Text>
+          </Button> */}
         </DataTable.Title>
         <DataTable.Title numeric>
           <Text style={styles.dataHeader}>Cost</Text>
@@ -43,7 +60,7 @@ export const OverviewTable = () => {
 
       {data.map((item, index) => {
         return (
-          <DataTable.Row key={item[0] + index}>
+          <DataTable.Row key={item[0] + index} style={{ borderBottomWidth: 0 }}>
             <DataTable.Cell>
               <Text style={styles.dataCell}>{item[0]}</Text>
             </DataTable.Cell>
