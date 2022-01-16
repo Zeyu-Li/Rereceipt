75fb345ac75e6543b183176d478dafa4a7a7f5eb
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-16 08:49:15 -0600

    fixed dialog picker

--- components/common/button.jsx
+++ components/common/button.jsx
@@ -8,22 +8,51 @@ import {
   Pressable,
   Text,
 } from "react-native";
-import { Button, Portal } from "react-native-paper";
+import { Button, Portal, RadioButton } from "react-native-paper";
 import { CenterContainer } from "../containers/GeneralContainer";
 
 export const PlusButton = () => {
   const [modalVisible, setModalVisible] = useState(false);
+
+  const changeSelection = (selection) => {
+    if (selection === "camera") {
+      console.log("Camera clicked");
+    } else if (selection === "file") {
+      console.log("file clicked");
+    } else {
+      console.log("manual clicked");
+    }
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.centeredView}>
       <Modal animationType="slide" transparent={true} visible={modalVisible}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Choose From Gallery</Text>
-            <Text style={styles.modalText}>Take Photo</Text>
-            <Text style={styles.modalText}>Manually Add Entry</Text>
+            <Text style={styles.modalText}>Add Entry</Text>
+            <RadioButton.Group
+              onValueChange={(value) => changeSelection(value)}
+            >
+              <RadioButton.Item
+                style={styles.modalText}
+                label="Camera"
+                value="Camera"
+              />
+              <RadioButton.Item
+                style={styles.modalText}
+                label="Upload File"
+                value="file"
+              />
+              <RadioButton.Item
+                style={styles.modalText}
+                label="Manual Entry"
+                value="manual"
+              />
+            </RadioButton.Group>
             <Pressable
               style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}
+              onPress={() => setModalVisible(false)}
             >
               <Text style={styles.textStyle}>Cancel</Text>
             </Pressable>
@@ -87,9 +116,15 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   button: {
-    borderRadius: 25,
+    borderRadius: 20,
     padding: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
     elevation: 2,
+    display: "flex",
+    alignContent: "flex-end",
+    justifyContent: "flex-end",
+    width: "100%",
   },
   buttonOpen: {
     backgroundColor: "#2196F3",
@@ -111,6 +146,7 @@ const styles = StyleSheet.create({
   },
   modalText: {
     marginBottom: 15,
+    fontSize: 20,
     textAlign: "center",
   },
 });
