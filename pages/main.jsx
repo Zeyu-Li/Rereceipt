30406238f16e32083e5315dbc775d9e54303db30
Zeyu-Li <zeyu.li23@gmail.com>
2022-01-15 20:16:03 -0600

    added gradient

--- pages/main.jsx
+++ pages/main.jsx
@@ -3,18 +3,29 @@ import { Text } from "react-native";
 import { OverviewTable } from "../components/common/Table";
 import {
   CenterContainer,
+  FancyContainer,
   GeneralContainer,
 } from "../components/containers/GeneralContainer";
 
 export const Main = () => {
   return (
     <>
+      <FancyContainer>
+        <CenterContainer>
+          {/* Chart */}
+          <Text>Chart</Text>
+        </CenterContainer>
+      </FancyContainer>
       <GeneralContainer>
         <CenterContainer>
-          <Text>Main 301</Text>
+          <Text>Search bar</Text>
         </CenterContainer>
       </GeneralContainer>
-      <OverviewTable />
+      <GeneralContainer>
+        <CenterContainer>
+          <OverviewTable />
+        </CenterContainer>
+      </GeneralContainer>
     </>
   );
 };
--- styles/styles.js
+++ styles/styles.js
@@ -1,22 +1,34 @@
 import { StyleSheet } from "react-native";
-import { secondary, text } from "./colors";
+import { primary, secondary, text } from "./colors";
 import { Dimensions } from "react-native";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
+const generalContainer = {
+  color: text,
+  padding: 15,
+  borderRadius: 15,
+  marginBottom: 20,
+  width: windowWidth * 0.8,
+};
+
 const styles = StyleSheet.create({
   generalContainer: {
     backgroundColor: secondary,
-    color: text,
-    padding: 15,
-    borderRadius: 15,
-    width: windowWidth * 0.8,
+    ...generalContainer,
   },
   center: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: primary,
+    color: text,
+    alignItems: "center",
+    padding: 35,
   },
 });
 
